7a6c7e31543e68152a02995a5f9755cf6d91b50c
Bogdan <c0pland@icloud.com>
2022-06-23 11:59:51 +0700

    Initial Commit

---
+++ ToDoey Desktop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ToDoey Desktop
+//
+//  Created by Bogdan Benner on 23.06.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
