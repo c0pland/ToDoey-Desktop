51a79db263b859281d306dbd488b70c7d0f8ae0f
Bogdan <c0pland@icloud.com>
2022-06-29 10:57:54 +0700

    add UI. add saving to Core Data

--- ToDoey Desktop/ViewController.swift
+++ ToDoey Desktop/ViewController.swift
@@ -6,21 +6,32 @@
 //
 
 import Cocoa
+import CoreData
 
 class ViewController: NSViewController {
-
+    
+    
+    @IBOutlet weak var importantCheckbox: NSButton!
+    @IBOutlet weak var taskField: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    @IBAction func addClicked(_ sender: NSButton) {
+        if taskField.stringValue.count > 0 {
+            if let context = (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let newTask = Task(context: context)
+                newTask.name = taskField.stringValue
+                newTask.isImportant = (importantCheckbox.state as NSNumber) != 0
+                do {
+                    try (NSApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext.save()
+                } catch {
+                    print("error saving, \(error)")
+                }
+            }
         }
     }
-
-
 }
 
